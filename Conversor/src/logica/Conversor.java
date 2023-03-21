3bd56241186698947946b845845cbdc8e09f25d8
Eduardo Cecchini <119954790+EduardoCecchini@users.noreply.github.com>
2023-03-21 17:43:26 -0300

    Primer commit
    Se agrega el proyecto entero con sus librerías externas

---
+++ Conversor/src/logica/Conversor.java
@@ -0,0 +1,14 @@
+
+package logica;
+
+import igu.pantalla_inicial;
+
+public class Conversor {
+    public static void main(String[] args){
+
+        pantalla_inicial abrir = new pantalla_inicial();
+        abrir.setVisible(true);
+        abrir.setLocationRelativeTo(null);
+        abrir.setResizable(false);
+    }
+}
